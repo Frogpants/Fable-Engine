e7cd0df34c8fcf908bb29fe582832e16bf561155
jm1021 <jmort1021@gmail.com>
2025-09-09 10:13:01 -0700

    updated main and shader files

--- src/code/graphics/Shader.java
+++ src/code/graphics/Shader.java
@@ -5,9 +5,8 @@ shader.bind();
 shader.setUniform("projection", camera.getProjectionMatrix());
 shader.setUniform("view", camera.getViewMatrix());
 shader.setUniform("model", entity.getTransform().getMatrix());
-shader.setUniform("triangleColor", new Vector3f(1, 0, 0)); // red triangle
+shader.setUniform("triangleColor", new Vector3f(1, 0, 0));
 
-// draw your mesh
 mesh.bind();
 GL11.glDrawElements(GL11.GL_TRIANGLES, mesh.getVertexCount(), GL11.GL_UNSIGNED_INT, 0);
 mesh.unbind();
